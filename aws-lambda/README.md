3dec6bc4185d084d892d65a78e6725094f69e3c9
CodeVsk <vynycyusvieira@gmail.com>
2024-11-18 22:45:33 -0300

    feat: sample lambda function.

---
+++ aws-lambda/README.md
@@ -0,0 +1,13 @@
+## Run command to set environment OS Linux.
+
+```bash
+$Env:GOOS="linux"
+```
+
+## Run command to build lambda function.
+
+```bash
+go build .
+```
+
+## Zip file generated.
---
+++ aws-lambda/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	fmt.Println("Lambda function is healthy!")
+
+	response := events.APIGatewayProxyResponse {
+		StatusCode: 200,
+	}
+
+	return response, nil
+}
