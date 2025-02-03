0f654dce2713765fdac6d32ad06be58862e5f371
CodeVsk <vynycyusvieira@gmail.com>
2025-02-02 22:52:12 -0300

    raw: add separators.

--- design-patterns/factory-method/main.go
+++ design-patterns/factory-method/main.go
@@ -6,9 +6,7 @@ type PaymentProcessor interface {
 	ProcessPayment(amout int64)
 }
 
-type PaymentFactory interface {
-	CreatePaymentProcessor() PaymentProcessor
-}
+//--------------------------
 
 type Paypal struct{}
 
@@ -16,10 +14,18 @@ func (p *Paypal) ProcessPayment(amout int64) {
 	fmt.Println("Process payment with paypal.")
 }
 
+//--------------------------
+
 type PaypalFactory struct {}
 
 func (p *PaypalFactory) CreatePaymentProcessor() PaymentProcessor {
 	return &Paypal{}
+}
+
+//--------------------------
+
+type PaymentFactory interface {
+	CreatePaymentProcessor() PaymentProcessor
 }
 
 func ProcessPayment(factory PaymentFactory, amount int64) {
